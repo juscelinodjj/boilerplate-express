c4ffd1efe2e54bddcd97c615de9a156888d5a92b
Juscelino <juscelinodjj@gmail.com>
2021-09-29 21:15:43 -0300

    Add now router to myApp.js

--- myApp.js
+++ myApp.js
@@ -29,7 +29,12 @@ app.get('/json', function (req, res) {
   res.json({'message': value});
 });
 
-
+app.get('/now', function(req, res, next) {
+  req.time = new Date().toString();
+  next();
+}, function(req, res) {
+  res.json({'time': req.time});
+});
 
 
 
